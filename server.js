8a48381fbff8300f9647fffb356cffa3b9ebdc4b
Gwendolyn <dr.sparkles.warfield@gmail.com>
2018-09-11 19:37:15 -0700

    remove unneeded server routes

--- server.js
+++ server.js
@@ -36,13 +36,4 @@ apiRouter(app);
 
 app.use(historyFallback());
 
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
-
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
